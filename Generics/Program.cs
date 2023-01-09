7e70b3d6356d39e6d21bef98aeea1368c2ac0900
Florin GUGUI <fgugui@pentalog.com>
2023-01-09 12:19:45 +0200

    Added the Generics and Linq demos.

---
+++ Generics/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int number1 = 5, number2 = 10;
+            double number3 = 7, number4 = 7.0;
+
+            Console.WriteLine("Hello, World!");
+            Calculator calculator = new Calculator();
+
+            if (calculator.CheckEquality<int>(number1, number2))
+            {
+                Console.WriteLine("Int numbers are equal");
+            }
+            else
+            {
+                Console.WriteLine("Int numbers are not equal");
+            }
+
+            if (calculator.CheckEquality<double>(number3, number4))
+            {
+                Console.WriteLine("Double numbers are equal");
+            }
+            else
+            {
+                Console.WriteLine("Double numbers are not equal");
+            }
+        }
+    }
+
+    public class Calculator
+    {
+        //public bool CheckEquality(int number1, int number2)
+        //{
+        //    return number1 == number2;
+        //}
+
+        //public bool CheckEquality(double number1, double number2)
+        //{
+        //    return number1 == number2;
+        //}
+
+        public bool CheckEquality<T>(T number1, T number2)
+        {
+            return number1.Equals(number2);
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace Linq
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            List<int> intList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var evenIntsLinqExpression = from x in intList
+                                         where x % 2 == 0
+                                         select x;
+
+            foreach (var evenInt in evenIntsLinqExpression)
+            {
+                Console.WriteLine(evenInt);
+            }
+
+            var evenIntsLinqMethod = intList
+                .Where(x => x % 2 == 0)
+                .Select(x => x);
+
+            foreach (var evenInt in evenIntsLinqMethod)
+            {
+                Console.WriteLine(evenInt);
+            }
+        }
+    }
+}
